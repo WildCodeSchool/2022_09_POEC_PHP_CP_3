3e772bbc7a8d06eff96815d9d943a7c447da1467
julie-jeancon-donon <dononjulie@gmail.com>
2022-11-30 12:19:19 +0100

    movedirection function

--- src/Controller/BoatController.php
+++ src/Controller/BoatController.php
@@ -22,4 +22,34 @@ class BoatController extends AbstractController
         $em->flush();
         return $this->redirectToRoute('map');
     }
+    #[Route('/direction/{direction}', name: 'moveDirection', requirements: ['direction' => '/|N|S|E|W|/i'])]
+    public function moveDirection(string $direction, BoatRepository $boatRepository, EntityManagerInterface $em): Response
+    {
+        $boat = $boatRepository->findOneBy([]);
+        $x = $boat->getCoordX();
+        $y = $boat->getCoordY();
+
+        if($direction === 'E'){
+            $x = $x +1;
+            
+        }
+        elseif($direction==='W'){
+            $x = $x -1;
+            
+        }
+        elseif($direction==='N'){
+            $y = $y -1;
+        
+        }
+        elseif($direction==='S'){
+            $y = $y +1;
+            
+        }
+
+        $boat->setCoordX($x);
+        $boat->setCoordY($y);
+
+        $em->flush();
+        return $this->redirectToRoute('map');
+    }
 }
---
+++ src/Service/MapManager.php
@@ -0,0 +1,13 @@
+<?php
+use App\Entity\Boat;
+use App\Repository\BoatRepository;
+use App\Repository\TileRepository;
+
+class MapManager {
+
+    public function tileExists(int $x, int $y): Boolean
+    {
+    
+    }
+
+}
