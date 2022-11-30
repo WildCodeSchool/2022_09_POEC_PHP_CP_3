d1f1e5c2c8cbf32209d75132273cfd7c66fa2819
td6904 <td665@pm.me>
2022-11-30 12:16:33 +0100

    Commit after co-ordinate details visible

--- src/Controller/BoatController.php
+++ src/Controller/BoatController.php
@@ -13,12 +13,42 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/boat')]
 class BoatController extends AbstractController
 {
-    #[Route('/move/{x}/{y}', name: 'moveBoat', requirements: ['x' => '\d+', 'y' => '\d+'])]
+
+    public const DIRECTIONS = [
+        'N' => [0, -1],
+        'E' => [1, 0],
+        'S' => [0, 1],
+        'W' => [-1, 0],
+    ];
+
+
+    /*#[Route('/move/{x}/{y}', name: 'moveBoat', requirements: ['x' => '\d+', 'y' => '\d+'])]
     public function moveBoat(int $x, int $y, BoatRepository $boatRepository, EntityManagerInterface $em): Response
     {
         $boat = $boatRepository->findOneBy([]);
         $boat->setCoordX($x);
         $boat->setCoordY($y);
+        $em->flush();
+        return $this->redirectToRoute('map');
+    }*/
+
+    #[Route('/move/{direction}', name: 'moveBoatDirection', requirements: ['direction' => '/|N|S|E|W|/i'])]
+    public function moveDirection(string $direction, BoatRepository $boatRepository, EntityManagerInterface $em): Response
+    {
+        $boat = $boatRepository->findOneBy([]);
+        $x = $boat->getCoordX();
+        $y = $boat->getCoordY();
+
+        if (!key_exists($direction, self::DIRECTIONS)) {
+            throw new exception('Unknown direction');
+        }
+
+        $x = $x + self::DIRECTIONS[$direction][0];
+        $y = $y + self::DIRECTIONS[$direction][1];
+
+        $boat->setCoordX($x);
+        $boat->setCoordY($y);
+        $em->persist($boat);
         $em->flush();
         return $this->redirectToRoute('map');
     }
