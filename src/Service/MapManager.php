a1dce2f5a1fa054bd877e93955a8f7fff60caea8
MehdiDAAJI <mehdidaaji@gmail.com>
2022-11-30 12:08:56 +0100

    first commit

--- src/Service/MapManager.php
+++ src/Service/MapManager.php
@@ -6,12 +6,4 @@ use App\Repository\TileRepository;
 
 class MapManager
 {
-    public function __construct(private TileRepository $tileRepository)
-    {
-    }
-
-    public function tileExists(int $x, int $y): bool
-    {
-        return $this->tileRepository->findOneBy(['coordX' => $x, 'coordY' => $y]) !== null;
-    }
 }
